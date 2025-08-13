9d48fbe6244ffb58c4a8c136cb5a629ce45c8119
Egor Kalinin <76444754+KaLiNkA-code@users.noreply.github.com>
2025-08-13 23:35:24 +0300

    qf

--- installator.py
+++ installator.py
@@ -207,6 +207,7 @@ def check_docker_gpu_support():
                 return False
         except:
             pass
+        
         # Проверяем наличие nvidia-docker
         try:
             result = subprocess.run(['docker', 'run', '--rm', '--runtime=nvidia', 'ubuntu:20.04', 'nvidia-smi'], 
